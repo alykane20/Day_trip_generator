51a968e16aaa9ea5491aefaacefe8811237075be
Aly Kane <Aly@macbook-air.myfiosgateway.com>
2022-03-15 16:53:02 -0400

    removed duplicate functions

--- day_trip_project.py
+++ day_trip_project.py
@@ -1,9 +1,10 @@
+import random
+
 destinations = ["Boston, MA", "Dublin, Ireland", "Costa Rica", "Kenya"]
 transportations = ["Car", "Private jet", "Train", "Yacht"]
 restaurants = ["McDonalds", "California Taco", "Smashburger", "Pizzagando"]
 entertainment = ["Patriots game", "Adele concert", "Jungle safari", "Duck boat tour"]
 
-import random
 
 def welcome_to_pick_your_trip():
     user_wants_trip = input("Would you like to go on a trip this weekend? y/n:")
@@ -12,63 +13,28 @@ def welcome_to_pick_your_trip():
     else:
         print("Enjoy your weekend at home!")
 
-def chose_destination(list_of_destinations):
-    location_chosen = (random.choice(destinations))
-    print(f"We have chosen: {location_chosen}")
-    location_ok_by_user = input("Is this ok? y/n:")
-    while location_ok_by_user != "y":
-        location_chosen = (random.choice(destinations))
-        print(f"How about: {location_chosen}")
-        location_ok_by_user = input("Is this ok? y/n:")
-    print("Awesome, let's figure out how to get there!")
-    return location_chosen
-
-
-def chose_transport(list_of_transportation):
-    transport_chosen = (random.choice(transportations))
-    print(f"We will travel by: {transport_chosen}")
-    transport_ok_by_user = input("Is this ok? y/n:")
-    while transport_ok_by_user != "y":
-        transport_chosen = (random.choice(transportations))
-        print(f"How about: {transport_chosen}")
-        transport_ok_by_user = input("Is this ok? y/n:")
-    print("Sounds good, now we can pick somewhere to eat")
-    return transport_chosen
-
-def chose_restaurant(list_of_restaurants):
-    restaurant_chosen = (random.choice(restaurants))
-    print(f"We have chosen to eat at: {restaurant_chosen}")
-    restaurant_ok_by_user = input("Is this ok? y/n:")
-    while restaurant_ok_by_user != "y":
-        restaurant_chosen = (random.choice(restaurants))
-        print(f"How about: {restaurant_chosen}")
-        restaurant_ok_by_user= input("Is this ok? y/n:")
-    print("Great! Time to be entertained!")
-    return restaurant_chosen
-    
-
-def chose_entertainment(list_of_entertainment):
-    entertainment_chosen = (random.choice(entertainment))
-    print(f"We have chosen to attend: {entertainment_chosen}")
-    entertainment_ok_by_user = input("Is this ok? y/n:")
-    while entertainment_ok_by_user != "y":
-        entertainment_chosen = (random.choice(entertainment))
-        print(f"How about: {entertainment_chosen}")
-        entertainment_ok_by_user = input("Is this ok? y/n:")
-    print("Sounds fun!")
-    return entertainment_chosen
-    
-    
+def chose_random_option(list):
+    option_chosen = (random.choice(list))
+    print(f"We have chosen: {option_chosen}")
+    option_ok_by_user = input("Is this ok? y/n:")
+    while option_ok_by_user != "y":
+        option_chosen = (random.choice(list))
+        print(f"No worries, how about: {option_chosen}")
+        option_ok_by_user = input("Is this ok? y/n:")
+    print("Awesome!")
+    return option_chosen
+  
 def build_your_weekend_trip():
     welcome_to_pick_your_trip()
-    final_destination = chose_destination(destinations)
-    final_transportation = chose_transport(transportations)
-    final_restaurant = chose_restaurant(restaurants)
-    final_entertainment = chose_entertainment(entertainment)
-    travel_choices = [final_destination, final_transportation, final_restaurant, final_entertainment]
+    final_destination = chose_random_option(destinations)
+    final_transportation = chose_random_option(transportations)
+    final_restaurant = chose_random_option(restaurants)
+    final_entertainment = chose_random_option(entertainment)
     print("Congrats! Here is what you've decided to do:") 
-    for choice in travel_choices:
-        print(choice)
+    print(f"Destination: {final_destination}")
+    print(f"Transportation: {final_transportation}")
+    print(f"Restaurant: {final_restaurant}")
+    print(f"Entertainment: {final_entertainment}")
     confirmation_user_choice = input("Does this trip look good? y/n:")
     while confirmation_user_choice != "y":
         print("Ok, let's start over and pick a trip that you like!")
